614697039c6ecd132d6e75acafa008d54363ade8
panyuxin <532756203@qq.com>
2025-06-04 14:24:36 +0800

    2

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import preprocess from 'svelte-preprocess'; // 预处理器
+import adapter from '@sveltejs/adapter-static'; // 静态适配器
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// Use Cloudflare adapter for Cloudflare Pages deployment
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
+  },
 };
-
-export default config;
